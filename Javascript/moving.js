42db7e2fa9656ede2ead8498b713280f033a97cc
tibordragan <59528105+tibordragan@users.noreply.github.com>
2022-08-24 17:48:24 +0200

    upload
    upload

---
+++ Javascript/moving.js
@@ -0,0 +1,31 @@
+// Scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+  origin: 'left', 
+  duration: 1000,
+  distance: '1rem',
+  delay: 200
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right', 
+  duration: 1000,
+  distance: '1rem',
+  delay: 200
+});
+
+sr.reveal('.animate-top', {
+  origin: 'top', 
+  duration: 1000,
+  distance: '1rem',
+  delay: 300
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom', 
+  duration: 1000,
+  distance: '1rem',
+  delay: 300
+});
